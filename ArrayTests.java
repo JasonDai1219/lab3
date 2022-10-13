bcd89382e43d86ad08d9c81bcc2597dc9a696094
JasonDai1219 <114436475+JasonDai1219@users.noreply.github.com>
2022-10-13 15:42:23 -0700

    Changes for ArrayExamples and Tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,25 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = {1,3,5};
+    int[] x = ArrayExamples.reversed(input2);
+    assertArrayEquals(new int[]{5,3,1}, x);
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input = {1,3,5,7,9};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(new int[]{9,7,5,3,1}, input);
+  }
+
+  @Test
+  public void testaverageWithoutLowest() {
+    double[] input = {1.0,1.0,2.0,2.0,2.0,2.0};
+    double average = ArrayExamples.averageWithoutLowest(input);
+    assertEquals(2.0, average, 0);
+  }
 }
